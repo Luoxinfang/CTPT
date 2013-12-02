1692996786442062335c3bee184fb9b47a3981fc
Luoxinfang <xf.radish@gmail.com>
2013-12-03 01:31:13 +0800

    modify: make to render different num

--- js/page/index.js
+++ js/page/index.js
@@ -12,7 +12,8 @@ define(function (require, exports, module) {
   var $con = $('.content tbody');
   var $res = $('.result tbody');
   var tplArray = [];
-
+  var dataNumArr = [10, 100, 1000,2000,3000, 10000/*, 10 * 10000, 20 * 1000*/];
+  var getData = require('page/json');
   var underscoreTpl = require('../../tpl/underscoreTpl.tpl');
   var microTpl = underscoreTpl;
   var jadeTpl = require('../../tpl/jadeTpl');
@@ -20,23 +21,23 @@ define(function (require, exports, module) {
   var closureTpl = require('../../tpl/closureTpl');
   tplArray.push({
     underscoreTpl: underscoreTpl,
-    costTime: 0
+    costTime: []
   });
   tplArray.push({
     microTpl: microTpl,
-    costTime: 0
+    costTime: []
   });
   tplArray.push({
     jadeTpl: jadeTpl,
-    costTime: 0
+    costTime: []
   });
   tplArray.push({
     doTTpl: doTTpl,
-    costTime: 0
+    costTime: []
   });
   tplArray.push({
-    closureTpl:closureTpl,
-    costTime: 0
+    closureTpl: closureTpl,
+    costTime: []
   });
 
   _.forEach(tplArray, function (tpl) {
@@ -46,126 +47,101 @@ define(function (require, exports, module) {
       '<td>' + keys[0] + '</td><td class="costTime">' + costTime + '</td><td>padding...</td>' +
       '</tr>')
   });
-  $.ajax({
-    url: 'http://localhost:63342/test/users',
-    /*    url:'http://localhost:3000/test/users',*/
-    method: 'get',
-    dataType: 'json',
-    success: function (data) {
-      _.forEach(tplArray, function (tpl, index) {
-        var startTime = +new Date,
-          costTime = 0;
-        var tplType = _.keys(tpl)[0];
-        var tplStr = _.values(tpl)[0];
-        var compiledTpl=null;
-        $con.html('<tr><td colspan="7" class="tip">begin to render'+tplType+'</td></tr>');
-        switch (tplType) {
-          case 'underscoreTpl':
-            compiledTpl = _.template(tplStr);
-            $con.html(compiledTpl({
-              rows: data
-            }));
-            costTime = (+new Date) - startTime;
-            tpl.costTime = costTime;
-            break;
-          case 'microTpl':
-            compiledTpl = tmpl(microTpl);
-            $con.html(compiledTpl({
-              rows: data
-            }));
-            costTime = (+new Date) - startTime;
-            tpl.costTime = costTime;
-            break;
-          case 'jadeTpl':
-            $con.html(jadeTpl({
-              rows: data
-            }));
-            costTime = (+new Date) - startTime;
-            break;
-          case 'doTTpl':
-            compiledTpl=doT.template(doTTpl);
-            $con.html(compiledTpl({
-              rows: data
-            }));
-            costTime = (+new Date) - startTime;
-            tpl.costTime = costTime;
-            break;
-          case 'closureTpl':
-            $con.html(CTPT.test({
-              rows: data
-            }));
-            costTime = (+new Date) - startTime;
-            tpl.costTime = costTime;
-            break;
-        }
-        $res.children('tr[data-type="' + tplType + '"]').children('td').eq(1).html(costTime);
-        $res.children('tr[data-type="' + tplType + '"]').children('td').eq(2).html('ok');
-        renderResult(tplArray);
-      });
-    },
-    error: function (err) {
-      console.log(err);
-    }
+  _.each(dataNumArr, function (num) {
+    var data = getData(num);
+    _.forEach(tplArray, function (tpl, index) {
+      var startTime = +new Date,
+        costTime = 0;
+      var tplType = _.keys(tpl)[0];
+      var tplStr = _.values(tpl)[0];
+      var compiledTpl = null;
+      $con.html('<tr><td colspan="7" class="tip">begin to render' + tplType + '</td></tr>');
+      switch (tplType) {
+        case 'underscoreTpl':
+          compiledTpl = _.template(tplStr);
+          $con.html(compiledTpl({
+            rows: data
+          }));
+          costTime = (+new Date) - startTime;
+          break;
+        case 'microTpl':
+          compiledTpl = tmpl(microTpl);
+          $con.html(compiledTpl({
+            rows: data
+          }));
+          costTime = (+new Date) - startTime;
+          break;
+        case 'jadeTpl':
+          $con.html(jadeTpl({
+            rows: data
+          }));
+          costTime = (+new Date) - startTime;
+          break;
+        case 'doTTpl':
+          compiledTpl = doT.template(doTTpl);
+          $con.html(compiledTpl({
+            rows: data
+          }));
+          costTime = (+new Date) - startTime;
+          break;
+        case 'closureTpl':
+          $con.html(CTPT.test({
+            rows: data
+          }));
+          costTime = (+new Date) - startTime;
+          break;
+      }
+      tpl.costTime.push(costTime);
+      $res.children('tr[data-type="' + tplType + '"]').children('td').eq(1).html(costTime);
+      $res.children('tr[data-type="' + tplType + '"]').children('td').eq(2).html('ok');
+    });
   });
-  function renderResult(data){
-    var tplType=[],renderTime=[];
-    _.each(data,function(tpl){
-      tplType.push(_.keys(tpl)[0]);
-      renderTime.push(_.values(tpl)[0]);
+
+  //$('.info').html(data.length + 'rows has been rendered');
+  renderResult(tplArray);
+  function renderResult(data) {
+    var tplType = [], renderTime = [],series=[];
+    _.each(data, function (tpl) {
+      var key=_.keys(tpl)[0];
+      tplType.push(key);
+      renderTime.push(tpl.costTime);
+      series.push({
+        name:key,
+        data:tpl.costTime
+      });
     });
     $('#result').highcharts({
-      chart: {
-        type: 'bar'
-      },
       title: {
-        text: 'The performance of templates'
-      },
-      xAxis: {
-        categories: tplType,
-        labels: {
-          rotation: -45,
-          align: 'right',
-          style: {
-            fontSize: '13px',
-            fontFamily: 'Verdana, sans-serif'
-          }
-        }
-      },
-      yAxis: {
-        min: 0,
-        title: {
-          text: 'Population (millions)'
-        }
+        text: 'client template performance test',
+        x: -20 //center
       },
       subtitle: {
-        text: 'author:xf.radish'
+        text: 'author: radish',
+        x: -20
+      },
+      xAxis: {
+        categories: dataNumArr
+      },
+      yAxis: {
+        title: {
+          text: 'time (ms)'
+        },
+        plotLines: [{
+          value: 0,
+          width: 1,
+          color: '#808080'
+        }]
       },
       tooltip: {
-        valueSuffix: ' millions'
-      },
-      plotOptions: {
-        bar: {
-          dataLabels: {
-            enabled: true
-          }
-        }
+        valueSuffix: 'ms'
       },
       legend: {
-        layout: 'horizontal',
+        layout: 'vertical',
         align: 'right',
-        verticalAlign: 'top',
-        x: -40,
-        y: 100,
-        floating: true,
-        borderWidth: 1,
-        backgroundColor: '#FFFFFF',
-        shadow: true
+        verticalAlign: 'middle',
+        borderWidth: 0
       },
-      credits: {
-        enabled: false
-      },
-      series: data
+      series: series
     });
   }
-
 });
--- js/page/json.js
+++ js/page/json.js
@@ -1,10 +1,27 @@
 /**
  * Created by luoxinfang on 13-12-2.
  */
-[{
-  id:1,
-  name:xf.radish,
-  firstName:radish,
-  lastName:xf,
-  usedName:['a','b','c','d','e']
-}]
+/*
+ [{
+ id:1,
+ name:xf.radish,
+ firstName:radish,
+ lastName:xf,
+ usedName:['a','b','c','d','e']
+ }]*/
+
+define(function (require, exports, module) {
+  module.exports = function(num){
+    var users = [];
+    for (var i = 0, l = num; i < l; i++) {
+      users.push({
+        id:i,
+        name:'xf.radish',
+        firstName:'radish',
+        lastName:'xf',
+        usedName:['a','b','c','d','e']
+      });
+    }
+    return users;
+  };
+});
